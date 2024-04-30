b0043599cd4809de387142148827f9d5bd72e468
kanstat <kanchan.sharma9722@gmail.com>
2024-04-30 14:21:09 +0530

    A new flask project is initialsed

---
+++ board/pages.py
@@ -0,0 +1,22 @@
+from flask import Blueprint,render_template
+
+
+bp = Blueprint("pages",__name__)
+
+@bp.route("/")
+def home():
+    return render_template("pages/base.html")
+
+
+@bp.route("/about")
+def about():
+    return render_template("pages/about.html")
+
+@bp.route("/create",methods=("GET","POST"))
+def create():
+    return render_template("posts/create.html")
+
+@bp.route("/posts")
+def posts():
+    posts = []
+    return render_template("posts/post.html",posts=posts)
